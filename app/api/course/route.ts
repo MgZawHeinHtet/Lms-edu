052c1a1be2fb40d482c223345eed12dba01c3d97
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2024-12-22 22:23:23 +0630

    prisma and mongodb setup

---
+++ app/api/course/route.ts
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        
+        const toRead = await db.course.findMany()
+        return NextResponse.json(toRead);
+    } catch (error) {
+        console.log({error});
+        
+    }
+  
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const getBody = await req.json();
+
+  try {
+    const toStoreData = await db.course.create({
+      data: {
+        title: getBody.title,
+        description: getBody.description,
+      },
+    });
+    return NextResponse.json(toStoreData);
+  } catch (error) {
+    console.log({ error });
+  }
+}
